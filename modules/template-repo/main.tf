b0cd9e79a93b1711c124b324bf33415ae6133ecc
Arturo Seijas <arturo.seijas@canonical.com>
2024-11-07 11:48:52 +0100

    Do not dismiss stale reviews

--- modules/template-repo/main.tf
+++ modules/template-repo/main.tf
@@ -36,7 +36,7 @@ resource "github_branch_protection" "template-protect" {
   }
 
   required_pull_request_reviews {
-    dismiss_stale_reviews           = true
+    dismiss_stale_reviews           = false
     require_code_owner_reviews      = true
     required_approving_review_count = 2
   }
